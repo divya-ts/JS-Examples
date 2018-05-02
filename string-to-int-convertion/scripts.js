5cf905126bcd9a8c4f8c1980471443a03e09bfb3
Divya T S <divya.ts75@gmail.com>
2018-05-02 22:33:11 +0530

    This is a simple time calculator

---
+++ string-to-int-convertion/scripts.js
@@ -0,0 +1,4 @@
+var htmlBadges = prompt('How many html badges do you have? ');
+var cssBadges = prompt('How many css badges do you have? ');
+var totalBadges = parseInt(htmlBadges) + parseInt(cssBadges);
+alert('Woww!!! You have ' + totalBadges + ' badges!');
